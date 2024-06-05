d15d46a007b0a3a38ec4594a90b96383617d19c5
Victor Pinedo <victorpinedomartinez@gmail.com>
2024-06-05 09:11:33 -0500

    actualizar configs

--- src/firebaseConfig.tsx
+++ src/firebaseConfig.tsx
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDd6cq_TQiTBvZr--Azdj3XXNbFgjTyWgA",
-  authDomain: "loan-tracker-14253.firebaseapp.com",
-  projectId: "loan-tracker-14253",
-  storageBucket: "loan-tracker-14253.appspot.com",
-  messagingSenderId: "428250568481",
-  appId: "1:428250568481:web:e4a1600f5f57177f9b5060",
+  apiKey: "AIzaSyD1SKumwaNQB8RaZUftyyz1l4duS8nPQPU",
+  authDomain: "react-cursos-187dc.firebaseapp.com",
+  projectId: "react-cursos-187dc",
+  storageBucket: "react-cursos-187dc.appspot.com",
+  messagingSenderId: "791208906607",
+  appId: "1:791208906607:web:e642a487ddf1b4b4f1a29a"
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
